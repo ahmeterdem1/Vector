d71457778f595983b4e7d3e45569417af6bbd32a
ahmeterdem1 <ahmetblk1035@gmail.com>
2025-01-18 15:48:54 +0300

    Implement the initial versions of set functions, did not test them yet so no built package/documentation

--- vectorgebra/array/api/set.py
+++ vectorgebra/array/api/set.py
@@ -1,13 +1,48 @@
 from ..ndarray import Array
+from typing import Tuple
+from collections import OrderedDict
 
-def unique_all():
-    pass
+def unique_all(x: Array) -> Tuple[Array, Array, Array, Array]:
+    values_ = list(OrderedDict.fromkeys(x.values))  # So that the relative order is preserved
+    indices_ = [-1] * len(values_)
+    counts_ = dict.fromkeys(values_, 0)
+    inverse_indices_ = [0] * x.size
 
-def unique_counts():
-    pass
+    # Find all required fields in O(size) time
+    value_index = 0
+    for i in range(x.size):
+        counts_[x.values[i]] += 1
+        inverse_indices_[i] = values_.index(x.values[i])
+        if x.values[i] == values_[value_index]:
+            indices_[value_index] = i  # Instead of appending, initialize the list in its full size and just index/assign here
+            value_index += 1
 
-def unique_inverse():
-    pass
+    values = Array(values_)
+    indices = Array(indices_)
+    inverse_indices = Array(inverse_indices_)
+    inverse_indices.reshape(x.shape)
+    counts = Array(list(counts_.values()))
 
-def unique_values():
-    pass
+    return values, indices, inverse_indices, counts
+
+def unique_counts(x: Array) -> Tuple[Array, Array]:
+    values_ = list(set(x.values))
+    counts_ = [x.values.count(v) for v in values_]
+
+    values = Array(values_)
+    counts = Array(counts_)
+
+    return values, counts
+
+def unique_inverse(x: Array):
+    values_ = list(set(x.values))
+    inverse_indices_ = [values_.index(x_val) for x_val in x.values]
+
+    values = Array(values_)
+    inverse_indices = Array(inverse_indices_)
+    inverse_indices.reshape(x.shape)
+
+    return values, inverse_indices
+
+def unique_values(x: Array) -> Array:
+    return Array(list(set(x.values)))
